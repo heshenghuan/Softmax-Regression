125a981822fdf5af187c98028fd1817fa683a51d
heshenghuan <heshenghuan@sina.com>
2015-10-24 14:26:51 +0800

    Added test file.

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Created on 14:13:08 2015-10-24
+
+@author: heshenghuan (heshenghuan@sina.com)
+http://github.com/heshenghuan
+"""
+
+import softmaxreg as sr
+
+case = sr.create(size=10,classNum=2)
+case.printinfo()
