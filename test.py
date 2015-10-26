d9630dfcf8fc48ca9686119221c3ec1a8014f4d0
heshenghuan <heshenghuan@sina.com>
2015-10-26 13:36:55 +0800

    Added new test case of minibatch train func

--- test.py
+++ test.py
@@ -16,8 +16,12 @@ case = sr.SoftmaxReg()
 case.read_train_file(r"train.data")
 case.loadFeatSize(784,10)
 case.printinfo()
-#print case.label_set
+# batch gradient descent
+# case.train_batch(max_iter=200,learn_rate=0.01,lamb=0.0,delta=0.2515)
+# stochastic gradient descent
 case.train_sgd(max_iter=200,learn_rate=0.01,lamb=0.0,delta=0.2515)
+# mini batch gradient descent
+# case.train_mini_batch(batch_num=10,max_iter=200,learn_rate=0.05,lamb=0.0,delta=0.2515)
 
 x_test, y_test = sr.make_data(r"test.data")
 
